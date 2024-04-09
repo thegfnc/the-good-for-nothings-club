b007c41e6edd9567351d90ec07e2fd4091bb3b73
Jason Desiderio <jason@thegoodfornothings.club>
2024-04-09 15:12:38 -0500

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react'
 import './styles/globals.css'
 import Header from '@/app/components/Header'
 import Footer from './components/Footer'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'The Good for Nothings Club',
@@ -28,6 +29,16 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <Script src='https://www.googletagmanager.com/gtag/js?id=G-3MCRMXW804' />
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3MCRMXW804');
+          `}
+        </Script>
       </body>
     </html>
   )
