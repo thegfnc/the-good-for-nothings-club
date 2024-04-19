1d0784ebca601012e45921d74373f3dd4e4fcbea
Jason Desiderio <jason@thegoodfornothings.club>
2024-04-19 11:22:14 -0500

    Add webflow redirects

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,113 @@ const nextConfig = {
   },
 
   // https://nextjs.org/docs/app/api-reference/next-config-js/redirects
+  // migration from webflow
   async redirects() {
-    return []
+    return [
+      {
+        source: '/projects/almost-automatic-good-looks-cover-no-way-no-how',
+        destination:
+          '/projects/no-way-no-how-almost-automatic-good-looks-cover',
+        permanent: true,
+      },
+      {
+        source: '/projects/blue-by-you-turnstile-cover-sluggish',
+        destination: '/projects/sluggish-blue-by-you-turnstile-cover',
+        permanent: true,
+      },
+      {
+        source: '/projects/braid-frame-canvas-25th-anniversary-tour',
+        destination: '/projects/braid-frame-and-canvas-25th-anniversary-tour',
+        permanent: true,
+      },
+      {
+        source: '/projects/brian-tell-the-lovemakers-studio-weekend',
+        destination: '/projects/the-loveshakers-studio-weekend',
+        permanent: true,
+      },
+      {
+        source: '/projects/ep1-sluggish',
+        destination: '/projects/sluggish-ep1',
+        permanent: true,
+      },
+      {
+        source: '/projects/ep2-sluggish',
+        destination: '/projects/sluggish-ep2',
+        permanent: true,
+      },
+      {
+        source: '/projects/ep3-sluggish',
+        destination: '/projects/sluggish-ep3',
+        permanent: true,
+      },
+      {
+        source: '/projects/ep4-sluggish',
+        destination: '/projects/sluggish-ep4',
+        permanent: true,
+      },
+      {
+        source: '/projects/heatonist',
+        destination: '/projects/heatonist-store-development-and-maintenance',
+        permanent: true,
+      },
+      {
+        source: '/projects/is-weed-legal-here',
+        destination:
+          '/projects/is-weed-legal-here-global-cannabis-legality-tracker',
+        permanent: true,
+      },
+      {
+        source: '/projects/jeff-rosenstock-hellmode-tour-2023',
+        destination: '/projects/jeff-rosenstock-hellmode-tour-2023',
+        permanent: true,
+      },
+      {
+        source: '/projects/laundry-day',
+        destination: '/projects/chris-donahue-laundry-day',
+        permanent: true,
+      },
+      {
+        source: '/projects/lifepacks-mvp',
+        destination: '/projects/lifepacks-community-created-product-guides',
+        permanent: true,
+      },
+      {
+        source: '/projects/michael-michael-motorcycle-sluggish',
+        destination: '/projects/sluggish-michael-michael-motorcycle',
+        permanent: true,
+      },
+      {
+        source: '/projects/stand-out-single-brian-tell-the-lovemakers',
+        destination: '/projects/the-loveshakers-stand-out-single',
+        permanent: true,
+      },
+      {
+        source: '/projects/teaser-video-for-stand-out-single-release',
+        destination: '/projects/the-loveshakers-stand-out-teaser-video',
+        permanent: true,
+      },
+      {
+        source: '/projects/the-original-invisible-skateboards',
+        destination:
+          '/projects/chris-donahue-the-original-invisible-skateboards',
+        permanent: true,
+      },
+      {
+        source: '/projects/uncle-chip',
+        destination: '/projects/chris-donahue-uncle-chip',
+        permanent: true,
+      },
+      {
+        source: '/projects/warm-weather-woman-single-brian-tell-the-lovemakers',
+        destination: '/projects/the-loveshakers-warm-weather-woman-single',
+        permanent: true,
+      },
+      {
+        source: '/projects/watching-the-news-single-brian-tell-the-lovemakers',
+        destination: '/projects/the-loveshakers-watching-the-news-single',
+        permanent: true,
+      },
+    ]
   },
 }
 
