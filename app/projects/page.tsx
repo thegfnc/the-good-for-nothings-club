27c62be553dafecd4407acf2e4fcb86bb8cda930
Jason Desiderio <jason@thegoodfornothings.club>
2024-06-20 15:28:41 -0500

    Mess with priority for portfolio images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -192,7 +192,7 @@ export default async function Projects({ searchParams }: ProjectsProps) {
                               className={`aspect-video w-full object-cover`}
                               sizes='(max-width: 1024px) 100vw, 50vw'
                               quality={90}
-                              priority={index < 3}
+                              priority={index < 2}
                             />
                           )}
                           <Link
