3645247c891b1b49add22588f4a27bbbf1722cd6
Jason Desiderio <jason@thegoodfornothings.club>
2024-06-20 17:19:40 -0500

    Revert priority prop removal

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -192,6 +192,7 @@ export default async function Projects({ searchParams }: ProjectsProps) {
                               className={`aspect-video w-full object-cover`}
                               sizes='(max-width: 1024px) 100vw, 50vw'
                               quality={90}
+                              priority={index < 3}
                             />
                           )}
                           <Link
