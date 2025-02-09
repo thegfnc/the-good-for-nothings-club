6b6fabc9790aae4dee82f85e18feb42e9434e4b8
Jason Desiderio <jason@thegoodfornothings.club>
2025-02-08 23:34:28 -0600

    Add mega menu to portfolio

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,11 @@ import { usePathname } from 'next/navigation'
 
 const MENU_ITEMS = [
   { href: '/', text: 'Home' },
-  { href: '/projects', text: 'Projects', showOnMobile: true },
+  {
+    href: '/projects',
+    text: 'Projects',
+    showOnMobile: true,
+  },
   { href: 'https://shop.thegoodfornothings.club/', text: 'Shop' },
   { href: '/about', text: 'About' },
   { href: '/contact', text: 'Contact', showOnMobile: true },
@@ -25,14 +29,18 @@ const MENU_ITEMS = [
 export default function Header() {
   const pathname = usePathname()
   const [isMenuSheetVisible, setIsMenuSheetVisible] = useState(false)
+  const [hoveredMenuItemIndex, setHoveredMenuItemIndex] = useState(-1)
 
   useEffect(() => {
     setIsMenuSheetVisible(false)
   }, [pathname])
 
   return (
-    <header className='text-center font-sans font-black uppercase md:px-8 md:pt-8 xl:px-16 xl:pt-16'>
-      <div className='bg-background m-auto grid max-w-(--page-max-width) grid-cols-[64px_1fr_1fr] border-b-2 border-black md:grid-cols-5 md:border-2'>
+    <header
+      className='relative text-center font-sans font-black uppercase md:px-8 md:pt-8 xl:px-16 xl:pt-16'
+      onMouseOut={() => setHoveredMenuItemIndex(-1)}
+    >
+      <div className='bg-background relative z-10 m-auto grid max-w-(--page-max-width) grid-cols-[64px_1fr_1fr] border-b-2 border-black md:grid-cols-5 md:border-2'>
         <Sheet open={isMenuSheetVisible} onOpenChange={setIsMenuSheetVisible}>
           <SheetTrigger className='flex cursor-pointer items-center justify-center transition-colors hover:bg-black/10 active:bg-black/20 md:hidden'>
             <Menu height='28px' width='28px' />
@@ -57,7 +65,7 @@ export default function Header() {
           </SheetContent>
         </Sheet>
 
-        {MENU_ITEMS.map(item => (
+        {MENU_ITEMS.map((item, index) => (
           <Link
             key={item.href}
             href={item.href}
@@ -65,10 +73,23 @@ export default function Header() {
               !item.showOnMobile && 'hidden md:block',
               'border-black px-4 py-5 transition-colors not-first-of-type:border-l-2 hover:bg-black/10 hover:no-underline active:bg-black/20 md:border-b-0 md:px-6 md:py-6 xl:px-8 xl:py-8'
             )}
+            onMouseOver={() => setHoveredMenuItemIndex(index)}
           >
             {item.text}
           </Link>
         ))}
+      </div>
+      <div
+        className={cn(
+          'bg-background pointer-events-none absolute top-full right-0 left-0 mx-auto grid max-w-(--page-max-width) -translate-y-full grid-cols-4 p-4 opacity-0 transition-all',
+          hoveredMenuItemIndex === 1 &&
+            'pointer-events-auto translate-y-0 opacity-100'
+        )}
+      >
+        <div>In Progress</div>
+        <div>Completed</div>
+        <div>Paused</div>
+        <div>Cancelled</div>
       </div>
     </header>
   )
