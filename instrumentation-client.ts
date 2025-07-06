c3bf77802d87816d5a8e220df25b4754a410dc54
Jason Desiderio <jason@thegoodfornothings.club>
2025-07-05 22:56:44 -0500

    Upgrade sentry

---
+++ instrumentation-client.ts
@@ -0,0 +1,30 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  enabled: process.env.NEXT_PUBLIC_VERCEL_ENV !== 'development',
+
+  dsn: 'https://b09e651f10614ec57d486de1b4422648@o4506369040187392.ingest.us.sentry.io/4507098361430016',
+
+  // Add optional integrations for additional features
+  integrations: [Sentry.replayIntegration()],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,34 +1,13 @@
 import * as Sentry from '@sentry/nextjs'
 
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    Sentry.init({
-      enabled: process.env.VERCEL_ENV !== 'development',
-
-      dsn: 'https://b09e651f10614ec57d486de1b4422648@o4506369040187392.ingest.us.sentry.io/4507098361430016',
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-
-      // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-      spotlight: process.env.NODE_ENV === 'development',
-    })
+    await import('./sentry.server.config')
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    Sentry.init({
-      enabled: process.env.VERCEL_ENV !== 'development',
-
-      dsn: 'https://b09e651f10614ec57d486de1b4422648@o4506369040187392.ingest.us.sentry.io/4507098361430016',
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    })
+    await import('./sentry.edge.config')
   }
 }
+
+export const onRequestError = Sentry.captureRequestError
