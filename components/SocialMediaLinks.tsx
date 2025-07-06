fde5791ffe84d9716dfdfbaa437b836752f19941
Jason Desiderio <jason@thegoodfornothings.club>
2025-07-06 00:30:52 -0500

    Add tiktok and threads to website

--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FaGithub, FaInstagram, FaSpotify } from 'react-icons/fa'
+import { FaGithub, FaInstagram, FaSpotify, FaTiktok, FaThreads } from 'react-icons/fa6'
 
 export default function SocialMediaLinks() {
   return (
@@ -10,6 +10,20 @@ export default function SocialMediaLinks() {
         className='transition-opacity hover:opacity-60 active:opacity-100'
       >
         <FaInstagram />
+      </Link>
+      <Link
+        href='https://www.threads.com/@thegfnc'
+        target='_blank'
+        className='transition-opacity hover:opacity-60 active:opacity-100'
+      >
+        <FaThreads />
+      </Link>
+      <Link
+        href='https://www.tiktok.com/@thegfnc'
+        target='_blank'
+        className='transition-opacity hover:opacity-60 active:opacity-100'
+      >
+        <FaTiktok />
       </Link>
       <Link
         href='https://open.spotify.com/user/31l4gvropwokzlmzymegi3vqa7py?si=c5fce32011494e91'
